1357aa702dba9d2231cfe860bc37e7e5bb68942b
Jeppe Andersen <jeppe@outlook.com>
2019-01-21 20:47:32 +0100

    Show elapsed operation time for producer

--- src/commands/produceRecordCommandHandler.ts
+++ src/commands/produceRecordCommandHandler.ts
@@ -1,6 +1,7 @@
 import * as kafka from "kafka-node";
 import * as vscode from "vscode";
 
+import { performance } from "perf_hooks";
 import { Client } from "../client";
 import { OutputChannelProvider } from "../providers/outputChannelProvider";
 
@@ -16,7 +17,8 @@ export class ProduceRecordCommandHandler {
         const channel = this.channelProvider.getChannel("Kafka Producer Log");
         channel.show(false);
 
-        channel.appendLine(`Producing record(s)...`);
+        channel.appendLine(`Producing record(s)`);
+        const startOperation = performance.now();
 
         producer.send([{
             topic,
@@ -24,19 +26,22 @@ export class ProduceRecordCommandHandler {
             key,
             messages,
         }], (error, result) => {
+            const finishedOperation = performance.now();
+            const elapsed = (finishedOperation - startOperation).toFixed(2);
+
             if (error) {
-                channel.appendLine("Failed to produce record(s)");
+                channel.appendLine(`Failed to produce record(s) (${elapsed}ms)`);
 
                 if (error.message) {
-                    channel.appendLine("Error: " + error.message);
+                    channel.appendLine(`Error: ${error.message}`);
                 } else {
-                    channel.appendLine("Error: " + error);
+                    channel.appendLine(`Error: ${error}`);
                 }
 
                 return;
             }
 
-            channel.appendLine(`Produced ${times} record(s)`);
+            channel.appendLine(`Produced ${times} record(s) (${elapsed}ms)`);
         });
     }
 
