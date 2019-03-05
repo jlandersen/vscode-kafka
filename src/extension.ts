24485a1d0b03f359a249581915f494ea462315a0
Jeppe Andersen <jeppe@outlook.com>
2019-03-05 19:53:33 +0100

    Make sure all disposables are registered

--- src/extension.ts
+++ src/extension.ts
@@ -19,53 +19,41 @@ export function activate(context: vscode.ExtensionContext) {
     const client = new Client({ host: settings.host });
     const explorer = new KafkaExplorer(client, settings);
     const outputChannelProvider = new OutputChannelProvider();
-    context.subscriptions.push(outputChannelProvider);
-
     const createTopicCommandHandler = new CreateTopicCommandHandler(client, explorer);
     const produceRecordCommandHandler = new ProduceRecordCommandHandler(client, outputChannelProvider);
     const startConsumerCommandHandler = new StartConsumerCommandHandler(client);
     const listConsumersCommandHandler = new ListConsumersCommandHandler();
     const toggleConsumerCommandHandler = new ToggleConsumerCommandHandler();
 
-    const settingsChangedHandlerDisposable = settings.onDidChangeSettings(() => {
+    context.subscriptions.push(settings.onDidChangeSettings(() => {
         client.refresh({ host: settings.host });
         explorer.refresh();
-    });
-
-    vscode.window.registerTreeDataProvider("kafkaExplorer", explorer);
-    vscode.commands.registerCommand("vscode-kafka.explorer.refresh", () => explorer.refresh());
-    vscode.commands.registerCommand(
-        "vscode-kafka.explorer.createtopic",
-        () => waitUntilConnected(client, () => createTopicCommandHandler.execute()));
-
-    vscode.commands.registerCommand(
-        "vscode-kafka.explorer.produce",
-        (document: vscode.TextDocument, range: vscode.Range, times: number) =>
-            waitUntilConnected(client, () => produceRecordCommandHandler.execute(document, range, times)));
-
-    vscode.commands.registerCommand(
-        "vscode-kafka.consumer.consume",
-        (topic: TopicItem | undefined) => {
-            waitUntilConnected(client, () => startConsumerCommandHandler.execute(topic));
-        });
-
-    vscode.commands.registerCommand(
-        "vscode-kafka.consumer.list",
-        () => {
-            listConsumersCommandHandler.execute();
-        });
-
-    vscode.commands.registerCommand(
-        "vscode-kafka.consumer.toggle",
-        () => {
-            toggleConsumerCommandHandler.execute();
-        });
-
-    context.subscriptions.push(settingsChangedHandlerDisposable);
+    }));
+    context.subscriptions.push(outputChannelProvider);
     context.subscriptions.push(client);
     context.subscriptions.push(explorer);
-
     context.subscriptions.push(new ConsumerStatusBarItem());
+    context.subscriptions.push(vscode.window.registerTreeDataProvider("kafkaExplorer", explorer));
+    context.subscriptions.push(vscode.commands.registerCommand(
+        "vscode-kafka.explorer.refresh",
+        () => explorer.refresh()));
+    context.subscriptions.push(vscode.commands.registerCommand(
+        "vscode-kafka.explorer.createtopic",
+        () => waitUntilConnected(client, () => createTopicCommandHandler.execute())));
+    context.subscriptions.push(vscode.commands.registerCommand(
+        "vscode-kafka.explorer.produce",
+        (document: vscode.TextDocument, range: vscode.Range, times: number) =>
+            waitUntilConnected(client, () => produceRecordCommandHandler.execute(document, range, times))));
+    context.subscriptions.push(vscode.commands.registerCommand(
+        "vscode-kafka.consumer.consume",
+        (topic: TopicItem | undefined) =>
+            waitUntilConnected(client, () => startConsumerCommandHandler.execute(topic))));
+    context.subscriptions.push(vscode.commands.registerCommand(
+        "vscode-kafka.consumer.list",
+        () => listConsumersCommandHandler.execute()));
+    context.subscriptions.push(vscode.commands.registerCommand(
+        "vscode-kafka.consumer.toggle",
+        () => toggleConsumerCommandHandler.execute()));
 
     const documentSelector = [
         { language: "kafka", scheme: "file" },
@@ -75,7 +63,6 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(
         vscode.languages.registerCodeLensProvider(documentSelector, new ProducerCodeLensProvider()));
-
     context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(
         ConsumerVirtualTextDocumentProvider.SCHEME,
         ConsumerVirtualTextDocumentProvider));
