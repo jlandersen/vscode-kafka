0d5867e43e0cd2d926487953ca64b588f1654009
Fred Bricon <fbricon@gmail.com>
2021-04-02 14:49:34 +0200

    Add color output to Mocha tests
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,4 +1,3 @@
-import * as assert from "assert";
 import { before } from "mocha";
 
 // You can import and use all API from the 'vscode' module
@@ -9,10 +8,5 @@ import * as vscode from "vscode";
 suite("Extension Test Suite", () => {
     before(() => {
         vscode.window.showInformationMessage("Start all tests.");
-    });
-
-    test("Sample test", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
     });
 });
